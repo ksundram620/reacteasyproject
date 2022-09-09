d6cf5a82fb919321de6001efceb8e51f089c336e
kumar sundram <104027247+ksundram620@users.noreply.github.com>
2022-09-09 15:06:33 +0530

    css and movie jsx change

--- src/movie.jsx
+++ src/movie.jsx
@@ -6,14 +6,14 @@ const Movie=(props)=>{
     <>
         {
             props.val.person===undefined?<div className="setmovieCss">
-        {props.val.show.image!=null?<img style={{width:"300px" ,height:"300px"}} src={props.val.show.image.medium
-}/>:<img style={{width:"300px" ,height:"300px"}} src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs="/>}
+        {props.val.show.image!=null?<img src={props.val.show.image.medium
+}/>:<img  src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs="/>}
          <p>{props.val.show.name}</p>
          
          
          </div>:<div className="setmovieCss">
-        {props.val.person.image!=null?<img style={{width:"300px" ,height:"300px"}} src={props.val.person.image.medium
-}/>:<img style={{width:"300px" ,height:"300px"}} src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs="/>}
+        {props.val.person.image!=null?<img src={props.val.person.image.medium
+}/>:<img src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs="/>}
         <p>{props.val.person.name}</p>
          </div>
         }
